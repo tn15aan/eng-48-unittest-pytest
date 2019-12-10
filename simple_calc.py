6361be6bc5529a8fffdf9590fb766502fcf56534
Thomas Nguyen <TNguyen@spartaglobal.com>
2019-12-10 10:29:22 +0000

    Added more tests

--- simple_calc.py
+++ simple_calc.py
@@ -1,10 +1,10 @@
 
 class SimpleCalc():
     def add(self, arg1, arg2):
-        return 6
+        return arg1 + arg2
     def subtract(self, arg1, arg2):
-        return 2
+        return arg1 - arg2
     def divide(self, arg1, arg2):
-        return 2
+        return arg1 / arg2
     def multiply(self, arg1, arg2):
-        return 6
+        return arg1 * arg2
--- test_unittest_simplecalc.py
+++ test_unittest_simplecalc.py
@@ -7,13 +7,17 @@ class Calctest(unittest.TestCase):
 
     def test_add(self):
         self.assertEqual(self.calc.add(2, 4), 6)
+        self.assertEqual(self.calc.add(4, 4), 8)
 
     def test_subtract(self):
-        self.assertEqual(self.calc.subtract(4, 2), 2)
+        self.assertEqual(self.calc.subtract(8, 2), 6)
+        self.assertEqual(self.calc.subtract(8, 4), 4)
 
     def test_multiply(self):
         self.assertEqual(self.calc.multiply(2, 3), 6)
+        self.assertEqual(self.calc.multiply(4, 2), 8)
 
     def test_divide(self):
         self.assertEqual(self.calc.divide(8, 4), 2)
+        self.assertEqual(self.calc.divide(6, 2), 3)
 
